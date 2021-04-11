b1fa8f6a5ca85c62232b55feda110a881f83a8e1
LNOXS07 <omar.shawqy@outlook.com>
2021-04-11 16:00:37 +0100

    Finished steps 2-6 Module 2: - Added property Type to BaseGradeBook -Created StandarGradeBook Class -Created StandardGradeBook Type -Created RankedGradeBook Class -Crated RankedGradeBook Type

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+
+        }
+
+        public RankedGradeBook()
+        {
+            Type = GradeBookType.Ranked;
+        }
+    }
+}
